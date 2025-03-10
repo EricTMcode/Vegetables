8675da0e2a43deb71aa5a4ad7f4a8bbeca90f9ca
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-10 13:49:48 +0100

    Initial Commit

---
+++ Vegetables/VegetablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegetablesApp.swift
+//  Vegetables
+//
+//  Created by Eric on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct VegetablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
