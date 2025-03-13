dea863fc8651df4774729759881f9383d6e2d023
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-13 14:13:32 +0100

    Add PhotoPicker to AddNoteScreen

--- Vegetables/Screens/AddNoteScreen.swift
+++ Vegetables/Screens/AddNoteScreen.swift
@@ -5,6 +5,7 @@
 //  Created by Eric on 11/03/2025.
 //
 
+import PhotosUI
 import SwiftUI
 import SwiftData
 
@@ -18,6 +19,9 @@ struct AddNoteScreen: View {
     @State private var noteTitle: String = ""
     @State private var noteBody: String = ""
 
+    @State private var selectedPhotoItem: PhotosPickerItem? = nil
+    @State private var uiImage: UIImage?
+
     private func saveNote() {
         let note = Note(title: noteTitle, body: noteBody)
         myGardenVegetable.notes?.append(note)
@@ -30,6 +34,31 @@ struct AddNoteScreen: View {
             TextField("Title", text: $noteTitle)
             TextEditor(text: $noteBody)
                 .frame(minHeight: 200)
+
+            HStack(spacing: 20) {
+                PhotosPicker(selection: $selectedPhotoItem, matching: .images, photoLibrary: .shared()) {
+                    ZStack {
+                        Circle()
+                            .fill(.blue.opacity(0.2))
+                            .frame(width: 60, height: 60)
+                        Image(systemName: "photo.on.rectangle")
+                            .font(.title)
+                            .foregroundStyle(.blue)
+                    }
+                }
+            }
+            .buttonStyle(.borderless)
+        }
+        .task(id: selectedPhotoItem) {
+            if let selectedPhotoItem {
+                do {
+                    if let data = try await selectedPhotoItem.loadTransferable(type: Data.self) {
+                        uiImage = UIImage(data: data)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
         .toolbar {
             
