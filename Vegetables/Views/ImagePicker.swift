88bc33f98acb65af94854f984809d6c30447afa8
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-13 15:06:36 +0100

    Create ImagePicker UIViewControllerRepresentable to take pictures with camera.

---
+++ Vegetables/Views/ImagePicker.swift
@@ -0,0 +1,54 @@
+//
+//  ImagePicker.swift
+//  Vegetables
+//
+//  Created by Eric on 13/03/2025.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+
+    @Environment(\.dismiss) private var dismiss
+    @Binding var image: UIImage?
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+    var sourceType: UIImagePickerController.SourceType = .camera
+
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+    }
+
+    class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var picker: ImagePicker
+
+        init(_ picker: ImagePicker) {
+            self.picker = picker
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+            if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                self.picker.image = uiImage
+            }
+
+            self.picker.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.picker.dismiss()
+        }
+    }
+}
