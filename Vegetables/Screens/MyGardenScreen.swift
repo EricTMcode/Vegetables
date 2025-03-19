395182b0024ccedb96b5a2f0fbc54b19a15bbc8c
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-19 14:08:26 +0100

    Implementing delete on MyGardenScreen

--- Vegetables/Screens/MyGardenScreen.swift
+++ Vegetables/Screens/MyGardenScreen.swift
@@ -11,23 +11,34 @@ import SwiftData
 struct MyGardenScreen: View {
 
     @Query private var myGardenVegetables: [MyGardenVegetable]
+    @Environment(\.modelContext) private var modelContext
+
+    private func deleteMyGardenVegetable(at offsets: IndexSet) {
+        offsets.forEach { index in
+            let myGardenVegetable = myGardenVegetables[index]
+            modelContext.delete(myGardenVegetable)
+            try? modelContext.save()
+        }
+    }
 
     var body: some View {
-        List(myGardenVegetables) { myGardenVegetable in
-            NavigationLink {
-                NoteListScreen(myGardenVegetable: myGardenVegetable)
-            } label: {
-                MyGardenCellView(myGardenVegetable: myGardenVegetable)
+        List {
+            ForEach(myGardenVegetables) { myGardenVegetable in
+                NavigationLink {
+                    NoteListScreen(myGardenVegetable: myGardenVegetable)
+                } label: {
+                    MyGardenCellView(myGardenVegetable: myGardenVegetable)
+                }
             }
+            .onDelete(perform: deleteMyGardenVegetable)
         }
         .listStyle(.plain)
         .navigationTitle("My Garden")
     }
 }
 
-#Preview {
+#Preview(traits: .sampleData) {
     NavigationStack {
         MyGardenScreen()
     }
-    .modelContainer(previewContainer)
 }
