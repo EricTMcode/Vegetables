1338a7680bb6c4a640fe5f65294745f895c12739
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-19 14:14:45 +0100

    Implementing delete on notes

--- Vegetables/Screens/NoteListScreen.swift
+++ Vegetables/Screens/NoteListScreen.swift
@@ -9,14 +9,26 @@ import SwiftUI
 import SwiftData
 
 struct NoteListScreen: View {
-    
+
+    @Environment(\.modelContext) private var modelContext
     let myGardenVegetable: MyGardenVegetable
     
     @State private var addNotePresented = false
-    
+
+    private func deleteNote(at offsets: IndexSet) {
+        offsets.forEach { index in
+            let note = myGardenVegetable.notes![index]
+            modelContext.delete(note)
+            try? modelContext.save()
+        }
+    }
+
     var body: some View {
-        List(myGardenVegetable.notes ?? []) { note in
-            NoteCellView(note: note, placeHolderImage: myGardenVegetable.vegetable.thumbnailImage)
+        List {
+            ForEach(myGardenVegetable.notes ?? []) { note in
+                NoteCellView(note: note, placeHolderImage: myGardenVegetable.vegetable.thumbnailImage)
+            }
+            .onDelete(perform: deleteNote)
         }
         .navigationTitle(myGardenVegetable.vegetable.name)
         .toolbar {
